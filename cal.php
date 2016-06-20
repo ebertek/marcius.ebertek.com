295d20ef2500fd5678539063fcf0c19835caf0c9
David Ebert <ebertek@gmail.com>
2016-06-20 14:27:02 +0200

    Tidy up + ?m

--- cal.php
+++ cal.php
@@ -1,24 +1,35 @@
 <?php
+  setlocale(LC_TIME, "hu_HU");
+  date_default_timezone_set('Europe/Budapest');
 
-  $lampaf = file_get_contents('./lampa/' . date('Y-m') .'.txt', NULL, NULL, 0, 31); // teljes honap 0/1
+  if (isset($_GET['m']) && $_GET['m'] != '') {
+    $lampam = $_GET['m'];
+  } else {
+    $lampam = date('Y-m');
+  }
+  $lampaf = file_get_contents('./lampa/' . $lampam .'.txt', NULL, NULL, 0, 31); // teljes honap 0/1
   $strlen = strlen($lampaf);
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="hu-HU" lang="hu-HU">
 
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta http-equiv="cache-control" content="no-cache" />
-    <meta name="Author" content="David Ebert" />
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Március - naptár</title>
     <meta name="Description" content="Calendar." />
-    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <meta http-equiv="cache-control" content="no-cache" />
     <meta name="robots" content="noindex, nofollow" />
-    <title>Március</title>
-    <link rel="home" href="http://www.ebertek.com/" />
-    <link rel="stylesheet" href="styles/marcius.css" type="text/css" />
-    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <meta name="googlebot" content="noindex, nofollow" />
+
+    <link href="https://ebertek.com/" rel="home" />
+    <link href="humans.txt" rel="author" type="text/plain" />
+    <link href="css/marcius.css" rel="stylesheet"  type="text/css" />
+    <link href="favicon.ico" rel="icon" type="image/x-icon" />
   </head>
 
   <body>
